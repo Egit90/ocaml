ea56c76a66b736ffaeab36de764e1d5b3d396924
Egit90 <elie90@gmail.com>
2023-10-13 16:06:31 +0200

    git ignore

--- leap/leap.ml
+++ leap/leap.ml
@@ -1,8 +1,2 @@
 let leap_year inYear = 
-    if inYear mod 4 == 0 then
-        if inYear mod 100 == 0 && inYear mod 400 <> 0 then
-            false
-        else 
-            true
-    else
-        false
+    (inYear mod 4 == 0) && (inYear mod 100 <> 0 || inYear mod 400 == 0)
